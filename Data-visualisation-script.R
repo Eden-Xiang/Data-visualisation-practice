3dd6c14d46b515dcad33c642303e3525b6c7347a
向一丹 <xiangyidan@macbook-pro.mynet>
2022-03-06 16:55:53 +0000

    aesthesic

--- Data-visualisation-script.R
+++ Data-visualisation-script.R
@@ -10,3 +10,9 @@ ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy, alpha = class))
 ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy, shape = class))          
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy), colour = "blue")
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, colour = class, stroke = 0.1))
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, colour = displ < 5))
