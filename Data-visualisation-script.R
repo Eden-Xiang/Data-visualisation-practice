88e3931c4997c6d6e93db8301615166ef560439e
向一丹 <xiangyidan@macbook-pro.mynet>
2022-03-06 16:36:00 +0000

    aesthetic

--- Data-visualisation-script.R
+++ Data-visualisation-script.R
@@ -6,4 +6,7 @@ ggplot(data = mpg) +
 
 ggplot(data = mpg) +
   geom_point(mapping = aes(x = displ, y = hwy, colour = class))
-             
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, alpha = class))
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, shape = class))          
