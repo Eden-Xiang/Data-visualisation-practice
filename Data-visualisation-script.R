226d5e79c99e0a2ace3cbd82616b900eca3f22e1
向一丹 <xiangyidan@macbook-pro.mynet>
2022-03-06 16:15:34 +0000

    mappings

--- Data-visualisation-script.R
+++ Data-visualisation-script.R
@@ -1,2 +1,9 @@
 library(tidyverse)
 mpg
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy))
+
+ggplot(data = mpg) +
+  geom_point(mapping = aes(x = displ, y = hwy, colour = class))
+             
